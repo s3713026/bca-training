edb45c165007d2ec613d6357cf3b7b586ec8f816
unknown <phucuong200297@gmail.com>
2022-03-11 14:25:51 +0700

    updatezalo1

---
+++ public/javascript/login.js
@@ -0,0 +1,3 @@
+function zaloLogin(){
+            window.location.replace('https://oauth.zaloapp.com/v4/oa/permission?app_id=3264157168871710467&redirect_uri=https%3A%2F%2Fmukokistore.herokuapp.com%2F&code_challenge=i6TPCNZbHRxIDvclgmT3ah8T0ff6eo6K6jIIn2t4uOU')
+}
