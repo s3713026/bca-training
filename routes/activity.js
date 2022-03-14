39bf213d38c5bdf06f773730d945d74e9ffca16c
Phu cuong Truong <phucuong200297@gmail.com>
2022-03-14 23:34:57 +0700

    training.9

--- routes/activity.js
+++ routes/activity.js
@@ -6,50 +6,50 @@ const fs = require('fs');
 
 
 
-/**
- * Endpoint that get long token.
- * @param req
- * @param res
- * @returns {Promise<void>}
- */
- exports.getToken = async (req,res) =>{
-  var url_page = req.query;
-     var string = JSON.stringify(url_page);
-     var objectValue = JSON.parse(string);
-     var get_authorization_code = objectValue['code'];
-     console.log("Authorization Code: " + get_authorization_code);
-    //  res.send(get_authorization_code);
+// /**
+//  * Endpoint that get long token.
+//  * @param req
+//  * @param res
+//  * @returns {Promise<void>}
+//  */
+//  exports.getToken = async (req,res) =>{
+//   var url_page = req.query;
+//      var string = JSON.stringify(url_page);
+//      var objectValue = JSON.parse(string);
+//      var get_authorization_code = objectValue['code'];
+//      console.log("Authorization Code: " + get_authorization_code);
+//     //  res.send(get_authorization_code);
  
-     var request = require('request');
-     var options = {
-         'method': 'POST',
-         'url': 'https://oauth.zaloapp.com/v4/oa/access_token',
-         'headers': {
-             'secret_key': 'q52K4eXpUtLN353SVUcN',
-             'Content-Type': 'application/x-www-form-urlencoded'
-         },
-         form: {
-             'app_id': '3264157168871710467',
-             'code_verifier': 'yWjvLbkuOMEZWUcMaPF43ChOldw8H87P_Zm813H5m1M',
-             'code': get_authorization_code,
-             'grant_type': 'authorization_code'
-         }
-     };
-     request(options, function (error, response) {
-         if (error) throw new Error(error);
-         console.log(response.body);
-         var infor = JSON.parse(response.body);
-         var ac_token = infor.access_token
-        //  res.send(ac_token)
-         fs.writeFile("config.json", JSON.stringify(ac_token), function (err) {
-             if (err) {
-                 return console.log(err);
-             }
-             console.log("The file was saved!");
-         });
-     });
+//      var request = require('request');
+//      var options = {
+//          'method': 'POST',
+//          'url': 'https://oauth.zaloapp.com/v4/oa/access_token',
+//          'headers': {
+//              'secret_key': 'q52K4eXpUtLN353SVUcN',
+//              'Content-Type': 'application/x-www-form-urlencoded'
+//          },
+//          form: {
+//              'app_id': '3264157168871710467',
+//              'code_verifier': 'yWjvLbkuOMEZWUcMaPF43ChOldw8H87P_Zm813H5m1M',
+//              'code': get_authorization_code,
+//              'grant_type': 'authorization_code'
+//          }
+//      };
+//      request(options, function (error, response) {
+//          if (error) throw new Error(error);
+//          console.log(response.body);
+//          var infor = JSON.parse(response.body);
+//          var ac_token = infor.access_token
+//         //  res.send(ac_token)
+//          fs.writeFile("config.json", JSON.stringify(ac_token), function (err) {
+//              if (err) {
+//                  return console.log(err);
+//              }
+//              console.log("The file was saved!");
+//          });
+//      });
  
- }
+//  }
 
 /**
  * The Journey Builder calls this method for each contact processed by the journey.
