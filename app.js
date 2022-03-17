9d87b499caf4643009380ffb22cba7677545bb29
unknown <phucuong200297@gmail.com>
2022-03-17 15:04:51 +0700

    config.json

--- app.js
+++ app.js
@@ -47,50 +47,9 @@ app.use('/journey/publish/', activityRouter.publish);
 app.use('/journey/validate/', activityRouter.validate);
 
 // serve UI
-// app.use('/',routes.ui);
+app.use('/',routes.ui);
 
-app.get('/',(res,req)=>{
-  var url_page = req.query;
-     var string = JSON.stringify(url_page);
-     console.log("Check bug:")
-     console.log(string);
-     var objectValue = JSON.parse(string);
-     var get_authorization_code = objectValue['code'];
-     console.log("Authorization Code: " + get_authorization_code);
-     var request = require('request');
-     var options = {
-         'method': 'POST',
-         'url': 'https://oauth.zaloapp.com/v4/oa/access_token',
-         'headers': {
-             'secret_key': 'q52K4eXpUtLN353SVUcN',
-             'Content-Type': 'application/x-www-form-urlencoded'
-         },
-         form: {
-             'app_id': '3264157168871710467',
-             'code_verifier': 'yWjvLbkuOMEZWUcMaPF43ChOldw8H87P_Zm813H5m1M',
-             'code': get_authorization_code,
-             'grant_type': 'authorization_code'
-         }
-     };
-     request(options, function (error, response) {
-         if (error) throw new Error(error);
-        //  console.log(response.body);
-         var infor = JSON.parse(response.body);
-         var ac_token = infor.access_token
-         console.log(ac_token);
-        //  res.send(ac_token)
-        if(ac_token!=undefined){
-         fs.writeFile("config.json", JSON.stringify(ac_token), function (err) {
-             if (err) {
-                 return console.log(err);
-             }
-             console.log("The file was saved!");
-         });
-        } else {
-          console.log("Have bọ here");
-        }
-     });
-})
+
 
 
 
