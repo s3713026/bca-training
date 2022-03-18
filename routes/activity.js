d95395b6d42723034afbc0ef96dac50da28a51f1
unknown <phucuong200297@gmail.com>
2022-03-18 12:15:18 +0700

    fix bọ

--- routes/activity.js
+++ routes/activity.js
@@ -122,6 +122,7 @@ exports.getUserInfor = async (res, req) => {
           'username': username,
           'user_ava': user_avatar
         }
+        console.log(userInfor);
         if (fs.existsSync("userinf.json")) {
           fs.readFile("userinf.json", 'utf8', (err, dataInf) => {
             if (err) {
