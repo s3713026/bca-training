ae27d38665bcf6890bfedc6282a8ad2edb9b47bc
unknown <phucuong200297@gmail.com>
2022-03-18 16:26:45 +0700

    bug

--- routes/index.js
+++ routes/index.js
@@ -38,8 +38,8 @@ exports.ui = (req, res) => {
         return
       }
       console.log("Đã lấy được thông tin người dùng")
-      console.log(data)
-      var user_infor = JSON.parse("["+data+"]")
+      console.log("["+data+"]")
+      var user_infor = JSON.parse(data)
       for (i in user_infor) {
         res.render('index', {
           title: 'Zalo Custom Activity',
