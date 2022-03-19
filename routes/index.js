e52e626de30203373e6ddc9d39a2dd6327e722cf
Phu cuong Truong <phucuong200297@gmail.com>
2022-03-19 22:37:29 +0700

    qqq

--- routes/index.js
+++ routes/index.js
@@ -90,7 +90,7 @@ exports.ui = async (req, res) => {
           }
         })
       )
-      then.res.render('index', {
+      .then.res.render('index', {
         title: 'Zalo Custom Activity',
         dropdownOptionsMessSend: [
           {
