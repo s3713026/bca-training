1e6f073023ecb8018a5d1dbe97bd4c68e24fa050
Phu cuong Truong <phucuong200297@gmail.com>
2022-03-20 23:33:08 +0700

    send mess

--- routes/index.js
+++ routes/index.js
@@ -56,8 +56,28 @@ const myPromise = new Promise((resolve, reject) => {
   }, 10000);
 });
 
-
-
+function sendMess (url,uid,text) {
+  var options = {
+    'method': 'POST',
+    'url': req.body.dropdownOptionsMessSend.value,
+    'headers': {
+        'access_token': acToken.token,
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "recipient": {
+            "user_id": req.body.dropdownOptionsClient.value
+        },
+        "message": {
+            "text": req.body.text
+        }
+    })
+};
+request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+});
+}
 
 /**
  * Render UI
