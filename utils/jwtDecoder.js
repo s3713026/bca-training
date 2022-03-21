49d7a5ddc50b37051f3f3e2dfa7ff0db1996b11b
unknown <phucuong200297@gmail.com>
2022-03-21 13:39:37 +0700

    fix it coi

--- utils/jwtDecoder.js
+++ utils/jwtDecoder.js
@@ -4,7 +4,7 @@ module.exports = (body) => {
   if (!body) {
     return new Error('invalid jwtdata');
   }
-  console.log("PRINT HERE", body);
+  console.log("PRINT HERE", body.toString());
   return JWT.verify(body.toString('utf8'), process.env.JWT, {
     algorithm: 'HS256',
   });
