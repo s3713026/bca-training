d5b575fd5f62aaa59c925918be355de79d8a381a
unknown <phucuong200297@gmail.com>
2022-03-21 16:11:35 +0700

    bug here

--- utils/jwtDecoder.js
+++ utils/jwtDecoder.js
@@ -2,10 +2,11 @@ const JWT = require('jsonwebtoken');
 
 module.exports = (body) => {
   if (!body) {
+    console.log("IN HỂ")
     return new Error('invalid jwtdata');
   }
   console.log("PRINT HERE", JSON.stringify(body));
   return JWT.verify(body.toString('utf8'), process.env.JWT, {
-    algorithm: 'HS256',
+    algorithm: 'HS384',
   });
 };
